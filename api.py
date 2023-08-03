5343720badaa10d3af9ab9b3deb0c0f1c10b295f
mikem1210 <140919878+mikem1210@users.noreply.github.com>
2023-08-03 14:18:55 -0400

    Add files via upload

---
+++ api.py
@@ -0,0 +1,39 @@
+#api.py
+from flask import Flask
+import sqlite3
+from flask_restful import Resource, Api
+
+DATABASE = 'todolist.db'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+api = Api(app)
+
+
+# Define the API resource for getting the to-do list items
+class ToDoListResource(Resource):
+    def get(self):
+        db = get_db()
+        cur = db.execute('SELECT what_to_do, due_date, status FROM entries')
+        entries = cur.fetchall()
+        tdlist = [dict(what_to_do=row[0], due_date=row[1], status=row[2]) for row in entries]
+        return tdlist
+
+
+api.add_resource(ToDoListResource, '/api/items')
+
+
+def get_db():
+    """Opens a new database connection if there is none yet for the
+    current application context.
+    """
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = sqlite3.connect(app.config['DATABASE'])
+    return g.sqlite_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database again at the end of the request."""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
